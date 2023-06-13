fdb29207e2da8dbf2862177c4240c7dc30407120
mrbrunelli <matheus.brunelli@gmail.com>
2023-06-13 16:40:15 -0300

    init spring project

---
+++ src/main/java/dev/mrbrunelli/springjavadelivery/SpringJavaDeliveryApplication.java
@@ -0,0 +1,13 @@
+package dev.mrbrunelli.springjavadelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJavaDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJavaDeliveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/mrbrunelli/springjavadelivery/SpringJavaDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mrbrunelli.springjavadelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJavaDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
