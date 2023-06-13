24b3fbfe73adc207487a24acd417a3420f1d577b
mrbrunelli <matheus.brunelli@gmail.com>
2023-06-13 18:16:01 -0300

    add save new product dto

---
+++ src/main/java/dev/mrbrunelli/springjavadelivery/product/SaveNewProductDTO.java
@@ -0,0 +1,31 @@
+package dev.mrbrunelli.springjavadelivery.product;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
+@Data
+public class SaveNewProductDTO {
+    @NotNull
+    private String name;
+
+    @NotNull
+    @Positive
+    private Double price;
+
+    @NotNull
+    @Positive
+    private Integer stock;
+
+    private Boolean isFeatured = false;
+
+    public Product toProduct() {
+        Product product = new Product();
+        product.setName(name);
+        product.setPrice(price);
+        product.setStock(stock);
+        product.setIsFeatured(isFeatured);
+        return product;
+    }
+}
